32acc7d79cf01e61b2998b51679d30d7a89a8c7e
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-01-19 13:31:45 -0500

    added properties for a game

--- MedievalWarfare/AbstractGame.cs
+++ MedievalWarfare/AbstractGame.cs
@@ -1,4 +1,10 @@
 abstract class AbstractGame
 {
+	// Used for when the game is loaded to identify which players
+	// were previously in the game. New game => empty list
+
+	// The assumption is that this is created when a lobby is made
+	// It should probably have a map and other things as well
+	ArrayList<Player> playersPreviouslyPlayer { get; };
 	int roundsPlayed { get; };
 }
