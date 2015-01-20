f4ffee73f063efd5213f2841b76a51950c775829
lordkevinthebold <kevinbourassahoule@gmail.com>
2015-01-20 10:18:34 -0500

    KBH - Networking files
    Scripts: Network, host and client
    Prefabs: Host, client
    Scene: WORKFILE

---
+++ Key Files/ClientPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class ClientPlayer : Player
+{	
+	// Use this for initialization
+	protected override void basicStart () 
+	{
+		isMyTurn = false;
+	}
+}
---
+++ Key Files/HostPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class HostPlayer : Player
+{
+	protected PhotonPlayer 	currentPlayer = PhotonNetwork.player;
+	protected int			currentPlayerIndex = 0;
+	
+	// Use this for initialization
+	protected override void basicStart () 
+	{
+		isMyTurn = true;
+	}
+
+	[RPC] void endTurn(PhotonPlayer playerEnded)
+	{
+		// Get next player
+		currentPlayerIndex = (currentPlayerIndex++) % PhotonNetwork.playerList.Length;
+		currentPlayer = PhotonNetwork.playerList[currentPlayerIndex];
+
+		// Tell him it is his turn
+		photonView.RPC("startTurn", currentPlayer, null);
+	}
+}
