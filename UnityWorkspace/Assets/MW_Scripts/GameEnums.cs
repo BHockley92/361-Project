803c0804e253a5d139a324fab0842791e14752b8
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-04 14:21:49 -0500

    added enums

---
+++ UnityWorkspace/Assets/MW_Scripts/GameEnums.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GameEnums
+{
+	enum PlayerStatus { Offline, Online };
+	enum LandType { Sea, Grass, Tree, Meadow };
+	enum UnitType { Peasant, Infantry, Soldier, Knight };
+	enum ActionType { ReadyForOrders, Moved, BuildingRoad, 
+		ChoppingTree, ClearingTombStone, UpgradingCombining, 
+		StartCultivating, FinishCultivating };
+
+	enum StructureType { NONE, Tombstone, Road, Tower };
+}
