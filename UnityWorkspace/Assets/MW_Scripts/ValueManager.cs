9fbf0406a34b52a67a92b9fc940c5698540b3143
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-06 11:04:26 -0500

    implemented getters to valueManager

--- UnityWorkspace/Assets/MW_Scripts/ValueManager.cs
+++ UnityWorkspace/Assets/MW_Scripts/ValueManager.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using GameEnums;
 
 public class ValueManager
 {
+	Dictionary <UnitType, int> 		unitValues;
+	Dictionary <VillageType, int> 	villageValues;
+	Dictionary <LandType, int>		landValues;
+
 	private string filePath { get; set; }
 	private int value;
 
 	public ValueManager();
 
-	public int getUnitValue(UnitType ut);
-	public int getLandValue(LandType lt);
+	public int getUnitValue(UnitType ut) { return unitValues [ut]; }
+	public int getLandValue(LandType lt) { return landValues [lt]; }
+	public int getVillageValue(VillageType vt) { return villageValues [vt]; }
+
+	private void loadValuesFromFile();
 
 }
