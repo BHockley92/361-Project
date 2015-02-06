bebe8eaed5810cd0b2892d7c2f445ba6c392149a
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-06 11:27:16 -0500

    added village and tile classes

---
+++ UnityWorkspace/Assets/MW_Scripts/Networking/Village.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using GameEnums;
+
+public class Village 
+{
+	private VillageType myType { get; set; }
+	private int gold { get; set; }
+	private int wood { get; set; }
+	private IList<Tile> controlledRegion { get; }
+	private IList<Unit> supportedUnits { get; }
+	private Tile location { get; } // TODO: initialize
+	Player myPlayer { get; set; } // TODO: initialize -- preferable to NOT have a setter and assign on construction?
+
+	public Village(IList<Tile> region) // Is there a true sequence interface/class?
+	{
+		myType = VillageType.Hovel;
+		gold = 0;
+		wood = 0;
+
+		controlledRegion = region;
+		supportedUnits = new List<Unit> ();
+	}
+
+	public void swapControl(Tile myTile, Village other);
+}
---
+++ UnityWorkspace/Assets/MW_Scripts/Tile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameEnums;
+
+public class Tile 
+{
+	private LandType myType { get; }
+	private Structure occupyingStructure { get; }
+	private Village myVillage{ get; set; }
+}
