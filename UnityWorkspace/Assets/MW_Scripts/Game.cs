76af3a422b090183f59e80f48039aff85c5f3df2
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-09 01:03:55 -0500

    added game class

---
+++ UnityWorkspace/Assets/MW_Scripts/Game.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game
+{
+	private List<Player> participants;
+	private Player turnOf { get; }
+
+	private GameLogic myGameLogic { get; }
+
+	// Global stats tracked, not for each player
+	private int roundsPlayed = 0;
+	private int unitsDiedFromCombat = 0;
+	private int unitsDiedFromPoverty = 0;
+
+	// TODO: board representation -- use this: http://www.redblobgames.com/grids/hexagons/#map-storage
+		// PROBLEM: specs don't specify dimensions of the board, it just says the island uses 300 tiles
+}
