7ed4d8e5669d05413a57b0fa6623f0e9789e2c39
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-10 10:48:39 -0500

    Added Coordinate Class to help with grid

---
+++ UnityWorkspace/Assets/MW_Scripts/Coordinate.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+// Helper class to be used to pass x and y coordinates
+public class Coordinate : IEqualityComparer<GridPoint>
+{
+	public int x { get; set; }
+	public int y { get; set; }
+
+	public Coordinate( int pX, int pY)
+	{
+		x = pX;
+		y = pY;
+	}
+
+	// All interface implementations for IEqualityComparer below
+	public override bool Equals(object obj)
+	{
+		if (obj == null) return false;
+		Coordinate objAsGP = obj as Coordinate;
+		if (objAsGP == null) return false;
+		return (this.x == objAsGP.x && this.y == objAsGP.y);
+	}
+	
+	public bool equals(Coordinate p)
+	{
+		return (this.x == p.x && this.y == p.y);
+	}
+	
+	public override int GetHashCode()
+	{
+		return this.x * 100 + this.y;
+	}
+	
+	public override string ToString ()
+	{
+		return "(" + x + ", " + y + ")";
+	}
+	
+	public bool Equals(Coordinate p1, Coordinate p2)
+	{
+		return p1.equals(p2);
+	}
+	
+	public int GetHashCode(Coordinate p)
+	{
+		return p.GetHashCode ();
+	}
+}
