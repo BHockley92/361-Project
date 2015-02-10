85602ca4e4e4c1271f7421452e4176e93535a7a7
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-10 01:30:28 -0500

    started implementing game class

--- UnityWorkspace/Assets/MW_Scripts/AbstractClasses/AbstractGame.cs
+++ UnityWorkspace/Assets/MW_Scripts/AbstractClasses/AbstractGame.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 
 public abstract class AbstractGame
 {
-	private List<Player> participants;
+	List<AbstractPlayer> participants;
 	private AbstractPlayer turnOf { get; set; }
 
-	private AbstractGameLogic myGameLogic { get; set; }
-	private AbstractTile[,] board { get; set; }
+	AbstractGameLogic myGameLogic { get; set; }
+	AbstractTile[,] board { get; set; }
 	// TODO: board representation -- use this: http://www.redblobgames.com/grids/hexagons/#map-storage
 
 	// Global stats tracked, not for each player
-	private int roundsPlayed = 0;
-	private int unitsDiedFromCombat = 0;
-	private int unitsDiedFromPoverty = 0;
+	int roundsPlayed = 0;
+	int unitsDiedFromCombat = 0;
+	int unitsDiedFromPoverty = 0;
 
 }
---
+++ UnityWorkspace/Assets/MW_Scripts/MW_Game.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using GameEnums;
+
+public class MW_Game 
+{
+	private IList<AbstractPlayer> participants;
+	public AbstractPlayer turnOf { get; private set; }
+	
+	public AbstractGameLogic myGameLogic { get; private set; }
+
+	// TODO: board representation -- use this: http://www.redblobgames.com/grids/hexagons/#map-storage
+	public AbstractTile[,] board { get; private set; }
+	
+	// Global stats tracked, not for each player
+	private int roundsPlayed = 0;
+	private int unitsDiedFromCombat = 0;
+	private int unitsDiedFromPoverty = 0;
+
+	// for a randomly generated board, assumes length and width are > 0
+	public MW_Game(IList<AbstractPlayer> players, int boardLength, int boardWidth)
+	{
+		participants = players;
+		board = generateRandomBoard (boardLength, boardWidth);
+	}
+
+	// Constructor for when a board is loaded
+	public MW_Game(IList<AbstractPlayer> players, AbstractTile[,] b)
+	{
+		participants = players;
+		turnOf = players [0];
+		board = b;
+	}
+
+	// Generates a random island that conforms to specs (300 land tiles that aren't water, one big land mass)
+	private AbstractTile[,] generateRandomBoard(int boardLength, int boardWidth)
+	{
+		// TODO
+		return null;
+	}
+}
