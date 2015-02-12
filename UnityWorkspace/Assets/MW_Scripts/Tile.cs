a2c3d8b77bbc785a8156f04a0c6275d7b31d1841
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-12 10:48:07 -0500

    started tile implementation

---
+++ UnityWorkspace/Assets/MW_Scripts/Tile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameEnums;
+
+public class Tile : AbstractTile 
+{
+	public LandType myType { get; private set; }
+	public AbstractStructure occupyingStructure { get; private set; }
+	public AbstractVillage myVillage{ get; private set; }
+	public AbstractUnit occupyingUnit { get; private set; }
+
+	public Tile( LandType type, AbstractVillage village)
+	{
+		myType = type;
+		myVillage = village;
+		occupyingStructure = null;
+		occupyingUnit = null;
+	}
+}
