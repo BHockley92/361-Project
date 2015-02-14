1dc2e0f8fc8da4401b1dd62a70221ab4cff04c5d
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-14 01:10:45 -0500

    started unit implementation

--- UnityWorkspace/Assets/MW_Scripts/AbstractClasses/AbstractUnit.cs
+++ UnityWorkspace/Assets/MW_Scripts/AbstractClasses/AbstractUnit.cs
@@ -7,6 +7,11 @@ public abstract class AbstractUnit
 	public UnitType myType { get; private set; }
 	public ActionType currentAction { get; private set; }
 	
-	public IList<AbstractVillage> myVillage { get; private set; }
+	public AbstractVillage myVillage { get; private set; }
 	public AbstractTile myLocation { get; private set; }
+
+	public PlayerPrefs getPlayer()
+	{
+		return myVillage.myPlayer;
+	}
 }
---
+++ UnityWorkspace/Assets/MW_Scripts/ImplementedClasses/Unit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : AbstractUnit 
+{
+	public Unit(AbstractVillage vOwner, AbstractTile startLocation)
+	{
+		// TODO: throw exception is try to make a unit?
+		myVillage = vOwner;
+		myLocation = startLocation;
+	}
+}
