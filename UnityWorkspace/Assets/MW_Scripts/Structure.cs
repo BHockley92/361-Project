3026003b48cffaed7445d713f24e335cb3ec1702
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-14 00:30:58 -0500

    implemented structure

---
+++ UnityWorkspace/Assets/MW_Scripts/Structure.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameEnums;
+
+public class Structure : AbstractStructure 
+{
+	public Structure( AbstractTile loc, StructureType t)
+	{
+		myLocation = loc;
+		myType = t;
+	}
+}
