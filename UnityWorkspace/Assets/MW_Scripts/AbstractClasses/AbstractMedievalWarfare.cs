e2f6adf365abf1d24a9ef966ebc740cb2c1d2f87
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-16 23:41:59 -0500

    updated abstractMW

--- UnityWorkspace/Assets/MW_Scripts/AbstractClasses/AbstractMedievalWarfare.cs
+++ UnityWorkspace/Assets/MW_Scripts/AbstractClasses/AbstractMedievalWarfare.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class AbstractMedievalWarfare
+public abstract class AbstractMedievalWarfare
 {
 	public AbstractGame currentGame { get; set; }
+	public abstract void newGame( List<AbstractPlayer> participants);
+	public abstract List<Tile> loadMaps(string myMapFile);
+	public abstract void assignVillages(List<Tile> myGameTiles);
+	public abstract void makeVillage(List<Tile> visitedTiles, Tile myTile, List<Player> participants);
 }
