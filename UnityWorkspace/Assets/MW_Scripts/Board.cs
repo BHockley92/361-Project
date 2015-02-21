83fc651921f999addeb97d78f1588739df021604
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-02-21 12:21:58 -0500

    board generation debugged: GREAT SUCCESS

--- UnityWorkspace/Assets/MW_Scripts/Board.cs
+++ UnityWorkspace/Assets/MW_Scripts/Board.cs
@@ -30,12 +30,10 @@ public class Board
 	}
 	
 	// Generates a random island that conforms to specs (300 land tiles that aren't water, one big land mass)
-	// TODO: debug
 	private void generateRandomBoard(int boardLength, int boardWidth)
 	{
 		board = new Tile[ boardLength, boardWidth ];
 		fillBoardWithWater ();
-
 		// Now make the island while taking into account for the border (don't put land there)
 		for(int i = 0 + border; i < board.GetLength(0) - border; i++)
 		{
@@ -66,8 +64,10 @@ public class Board
 		}
 	}
 
-	// returns neighbours of a given tile
-	// returns null if the tile is not found
+	/*
+	 * returns neighbours of a given tile
+	 * returns null if the tile is not found
+	 */ 
 	public List<Tile> getNeighbours(AbstractTile t)
 	{
 		for(int i = 0; i < board.GetLength(0); i++ )
@@ -76,7 +76,7 @@ public class Board
 			{
 				if( board[i, j] == t)
 				{
-					return getNeighbours(new Coordinate(i, j));
+					return getNeighbours(i, j);
 				}
 			}
 		}
@@ -84,51 +84,54 @@ public class Board
 		// tile wasn't found
 		return null;
 	}
-	
-	// get the neighbours of a hex tile
-	public List<Tile> getNeighbours(Coordinate c)
+
+	/**
+	 * Returns a List of tiles adjacent to the unit.
+	 * Returns an empty list if the coordinates are out of bounds.
+	 */
+	public List<Tile> getNeighbours(int x, int z)
 	{
 		List<Tile> ret = new List<Tile> ();
 
 		// Check that the coordinate is within bounds
-		if(c.x >= 0 && c.z >= 0)
+		if(x >= 0 && z >= 0)
 		{
-			if( c.x < board.GetLength(0) && c.z < board.GetLength(0))
+			if( x < board.GetLength(0) && z < board.GetLength(0))
 			{
 				// Now we know the coordinate is valid
 
 				// Check that left adjacent spaces are valid
-				if( c.x - 1 >= 0 )
+				if( x - 1 >= 0 )
 				{
-					ret.Add(board[c.x - 1, c.z]);
+					ret.Add(board[x - 1, z]);
 
 					// Check for the above and below
-					if( c.z - 1 >= 0 )
+					if( z - 1 >= 0 )
 					{
-						ret.Add(board[c.x - 1, c.z - 1]);
+						ret.Add(board[x - 1, z - 1]);
 					}
 
-					if( c.z + 1 < board.GetLength(1))
+					if( z + 1 < board.GetLength(1))
 					{
-						ret.Add(board[ c.x - 1, c.z + 1 ]);
+						ret.Add(board[ x - 1, z + 1 ]);
 					}
 				}
 
 				// Now check the right adjacent spaces
-				if( c.x + 1 < board.GetLength(0))
+				if( x + 1 < board.GetLength(0))
 				{
-					ret.Add ( board[ c.x + 1, c.z ] );
+					ret.Add ( board[ x + 1, z ] );
 
 					// Check for the above and below
 					// Check for the above and below
-					if( c.z - 1 >= 0 )
+					if( z - 1 >= 0 )
 					{
-						ret.Add(board[c.x + 1, c.z - 1]);
+						ret.Add(board[x + 1, z - 1]);
 					}
 					
-					if( c.z + 1 < board.GetLength(1))
+					if( z + 1 < board.GetLength(1))
 					{
-						ret.Add(board[ c.x + 1, c.z + 1 ]);
+						ret.Add(board[ x + 1, z + 1 ]);
 					}
 				}
 			}
