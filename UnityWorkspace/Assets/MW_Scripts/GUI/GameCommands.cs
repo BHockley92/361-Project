b11bcf7c4e5ecaa38b547954da76d99d0097c465
AlexSincennes <alexandre.sincennes@gmail.com>
2015-03-09 00:39:52 -0400

    clean up, part 2

---
+++ UnityWorkspace/Assets/MW_Scripts/GUI/GameCommands.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCommands : MonoBehaviour {
+
+	//Exit to desktop/quit button
+	public void ExitApp() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+	//Loads lobby info of selected game
+	public void JoinGame() {
+
+	}
+
+	//Populate popup with available maps
+	public void NewGame() {
+
+	}
+
+	//Populate popup with saved maps
+	public void LoadGame() {
+
+	}
+
+	//Create a lobby and populate with information
+	public void HostGame() {
+
+	}
+
+	//Sends message in input to all players
+	public void SendMessage() {
+
+	}
+
+	//Terminates the current game and sends to main menu
+	public void EndGame() {
+
+	}
+
+	//Saves the current state of the game in XML format and informs all players
+	public void SaveGame() {
+
+	}
+
+	//Ends current turn and goes to next player
+	public void EndTurn() {
+
+	}
+
+	//The ready or start button depending on host or player
+	public void Ready_Start() {
+
+	}
+
+	//The leave or disband button depending on host or player
+	public void Leave_Disband() {
+
+	}
+}
---
+++ UnityWorkspace/Assets/MW_Scripts/GUI/InGameMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenu : MonoBehaviour {
+
+	public CanvasGroup menu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp (KeyCode.Escape)) {
+			menu.alpha = 1;
+			menu.blocksRaycasts = true;
+			menu.interactable = true;
+		}
+	}
+}
