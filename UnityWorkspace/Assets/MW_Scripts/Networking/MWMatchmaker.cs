89a21cbae0cae0e9af22136e4959ab049cc1fd09
lordkevinthebold <kevinbourassahoule@gmail.com>
2015-03-10 00:41:07 -0400

    Put networking classes back so I can work on them

---
+++ UnityWorkspace/Assets/MW_Scripts/Networking/MWMatchmaker.cs
@@ -0,0 +1,18 @@
+/*
+ * This is a utility class for communication with the Matchmaking server.
+ */
+ public class MWMatchmaker
+ {
+	/*
+	 * Logs a user into the Matchmaker.  
+	 * Returns success or failure for logging in.
+	 */
+	public static bool login(String username, String password)
+	{
+		return false;
+	}
+	
+	/*
+	 * 
+	 */
+}
