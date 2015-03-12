f810ce707a0afc71b5ff620540173f4b8899c149
lordkevinthebold <kevinbourassahoule@gmail.com>
2015-03-11 21:05:05 -0400

    Network turns : CONFIRMED

--- UnityWorkspace/Assets/MW_Scripts/Networking/MWNetwork.cs
+++ UnityWorkspace/Assets/MW_Scripts/Networking/MWNetwork.cs
@@ -8,29 +8,35 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 /*
  * This is a utility class for networking within the Medieval Warfare game.
  */
-public class MWNetwork
+public class MWNetwork : Photon.MonoBehaviour
 {
-	private static string 	version;
+    // The game version.  Will probably never change this.
+    private static string version = "1.0";
 
-	// The constructor is private, as this is a utility class.
-	private MWNetwork() 
-	{
-		version 		= "1.0";	// TODO verify if valid version number
-	}
-	
+    // The GameObject associated to a player in MW.  It will only be instantiated if not null.
+    public GameObject player;
+
+    /* 
+     * Returns the MWNetwork component upon which you can call all functions below.
+     */
+    public static MWNetwork getInstance()
+    {
+        return GameObject.Find("MWNetwork").GetComponent<MWNetwork>();
+    }
+
 	/* 
 	 * Connects to Photon's master server using current version number.
 	 */
-	void Start() 
-	{
-		PhotonNetwork.ConnectUsingSettings(version);
-		Debug.Log("Connected to master server!");
-	}
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(version);
+        Debug.Log("Connected to master server!");
+    }
 	
 	/*
 	 * Sets a maximum number of players that can join a room/game.
 	 */
-	public void setMaxPlayers(int max)
+    public void setMaxPlayers(int max)
 	{
 		PhotonNetwork.room.maxPlayers = max;
 	}
@@ -38,7 +44,7 @@ public class MWNetwork
 	/*
 	 * Returns whether it is this player's turn or not.
 	 */
-	public static bool isMyTurn()
+	public bool isMyTurn()
 	{
 		return (bool) PhotonNetwork.player.customProperties["isMyTurn"];
 	}
@@ -46,39 +52,44 @@ public class MWNetwork
 	/*
 	 * Creates a room on the network with the given name.
 	 */
-	public static void createRoom(string roomName)
+	public void hostRoom(string roomName)
 	{
-		PhotonNetwork.CreateRoom(roomName, new RoomOptions() {maxPlayers = 2}, null);
-		
-		Hashtable roomProps = new Hashtable();
-		roomProps.Add("gameStarted", false);
-		PhotonNetwork.room.SetCustomProperties(roomProps);
+        PhotonNetwork.CreateRoom(roomName, new RoomOptions() { maxPlayers = 4 }, null);
 	}
 	
 	/*
 	 * Joins a room on the network with the given name.
 	 */
-	public static void joinRoom(string roomName)
+	public void joinRoom(string roomName)
 	{
 		PhotonNetwork.JoinRoom(roomName);
 	}
 	
 	/*
-	 * Makes the player join a room.
 	 * Initializes all player properties for the beginning of a game.
 	 * Currently, master client is automatically player 1 and other players turns
 	 * are ordered according to when they joined.
 	 */
-	public static void startGame()
+	public void startGame()
 	{
 		// Make sure starting conditions are satisfied:
-		// 1.	Game is not already in play.
+        // 1.   A room has been joined
+        if (PhotonNetwork.room == null)
+        {
+            Debug.Log("Cannot start game: a room has not yet been created");
+        }
+        // 2.   You are the host
+        if (!PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("Cannot start game: you are not the host.");
+        }
+		// 2. 	Game is not already in play.
 		if ((bool) PhotonNetwork.room.customProperties["gameStarted"])
 		{
 			Debug.Log("Cannot start game: it has already started.");
 			return;
 		}
-		// 2.	2 <= number of players <= maximum players
+		// 3.	2 <= number of players <= maximum players
 		if (PhotonNetwork.playerList.Length < 2 
 			|| PhotonNetwork.playerList.Length > PhotonNetwork.room.maxPlayers)
 		{
@@ -110,11 +121,44 @@ public class MWNetwork
 		roomProps.Add("gameStarted", true);
 		PhotonNetwork.room.SetCustomProperties(roomProps);
 	}
+
+    /*
+     * Returns whether or not you have joined a game.
+     */ 
+    public bool joinedRoom()
+    {
+        return PhotonNetwork.room != null;
+    }
+
+    /*
+    * Returns whether or not you have started the game.
+    */ 
+    public bool gameStarted()
+    {
+        return (bool) PhotonNetwork.room.customProperties["gameStarted"];
+    }
+
+    /*
+     * Returns whether or not you are the host
+     */
+    public bool isHost()
+    {
+        return PhotonNetwork.isMasterClient;
+    }
+
+    /*
+     * Returns whether or not you are connected to Photon's master server.
+     */ 
+    public bool connected()
+    {
+        return PhotonNetwork.connected;
+    }
 	
 	/*
 	 * Ends your turn, starting the next player's turn.
+     * TODO change order in which players play?
 	 */
-	public static void endTurn()
+	public void endTurn()
 	{
 		// End your turn
 		Hashtable property = new Hashtable();
@@ -132,7 +176,7 @@ public class MWNetwork
 	/*
 	 * Returns an array of room names.
 	 */
-	public static string[] getRoomList()
+	public string[] getRooms()
 	{
 		RoomInfo[] rooms = PhotonNetwork.GetRoomList();
 		
@@ -149,7 +193,7 @@ public class MWNetwork
 	/*
 	 * Returns an array of player names.
 	 */
-	public static string[] getPlayerList()
+	public string[] getPlayers()
 	{
 		PhotonPlayer[] players = PhotonNetwork.playerList;
 		
@@ -173,5 +217,24 @@ public class MWNetwork
         /*
          * As it is for the demo, the game state should update automatically.
          */ 
+    }
+
+    /*****************************************************************************************************
+     * The following functions implement pertinent Photon callbacks to react to certain networking events.
+     *****************************************************************************************************/
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("Connected to Room.");
+
+        if (player != null)
+            PhotonNetwork.Instantiate(player.name, new Vector3(-2 + PhotonNetwork.playerList.Length, 3, 0), Quaternion.identity, 0, null);
+    }
+
+    void OnCreatedRoom()
+    {
+        Hashtable roomProps = new Hashtable();
+        roomProps.Add("gameStarted", false);
+        PhotonNetwork.room.SetCustomProperties(roomProps);
     }
 }
---
+++ UnityWorkspace/Assets/MW_Scripts/Networking/MWNetwork_UnitTest/MWNetwork_UnitTest_Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MWNetwork_UnitTest_Player : Photon.MonoBehaviour 
+{
+    MWNetwork network;
+
+    void Start()
+    {
+        network = MWNetwork.getInstance();
+    }
+
+    void Update()
+    {
+        if (network.gameStarted() && network.isMyTurn() && photonView.isMine)
+            InputMovement();
+    }
+
+    void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.forward * 10 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.forward * 10 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.right * 10 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.right * 10 * Time.deltaTime);
+    }
+
+    void OnGUI()
+    {
+        if (network != null && network.gameStarted() && network.isMyTurn())
+        {
+            if (GUI.Button(new Rect(100, 100, 250, 100), "End Turn"))
+            {
+                network.endTurn();
+            }
+        }
+    }
+}
