09a6d19366cd8023c89e5aa12daf3750a8e4143c
Benjamin Hockley <benjamin.hockley@mail.mcgill.ca>
2015-03-31 23:38:16 -0400

    XmlLoad change
    Possible bug fix

--- UnityWorkspace/Assets/MW_Scripts/GUI/GUILogic.cs
+++ UnityWorkspace/Assets/MW_Scripts/GUI/GUILogic.cs
@@ -76,7 +76,7 @@ public class GUILogic : MonoBehaviour {
 
 	public void UpdateGameState(string gameState, int senderId) {
 		XmlDocument state = new XmlDocument();
-		state.LoadXml(gameState); 
+		state.Load(gameState); 
 		GAME.gameBoard = SERIALIZER.loadGameState(state, GAME);
 		Debug.Log ("Received a state");
 		if (GAME.turnOf.username.Equals(NETWORK.GetLocalPlayerName()))
