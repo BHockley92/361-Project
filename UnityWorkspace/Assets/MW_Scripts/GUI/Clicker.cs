50d95bae384a26fa1bdee1d875c4e0b3a528dbf9
AlexSincennes <alexandre.sincennes@hotmail.com>
2015-04-06 19:28:08 -0400

    Clicking Visualization
    We need to fix bounding boxes eventually

--- UnityWorkspace/Assets/MW_Scripts/GUI/Clicker.cs
+++ UnityWorkspace/Assets/MW_Scripts/GUI/Clicker.cs
@@ -5,6 +5,8 @@ public class Clicker : MonoBehaviour
 {	
 	public GUILogic guiLogic;
 	
+	private Vector3 arrow_v = new Vector3(0.0f,0.5f,0.0f);
+	
 	void Start () {
 		guiLogic = GameObject.Find("GUILogic").GetComponent<GUILogic>();
 	}
@@ -13,7 +15,12 @@ public class Clicker : MonoBehaviour
 	// GAME OBJECT FOR THIS TO WORK
 	void OnMouseOver(){
 		if(Input.GetMouseButtonDown(0)){
+			// destroy previous "selected" effect
+			Destroy(GameObject.Find("SelectionArrow(Clone)"));
+			GameObject sa = Instantiate(Resources.Load("SelectionArrow")) as GameObject;
+			sa.transform.position = this.transform.position + arrow_v;
 			guiLogic.LAST_CLICKED_ON = this.transform;
+
 		}
 	}
 }
