b90226a348ca43e6ca952a0ccfab698aef00ab1b
AlexSincennes <alexandre.sincennes@hotmail.com>
2015-04-06 11:36:45 -0400

    Make code compile (Ben?)

--- UnityWorkspace/Assets/MW_Scripts/GUI/GUILogic.cs
+++ UnityWorkspace/Assets/MW_Scripts/GUI/GUILogic.cs
@@ -92,7 +92,7 @@ public class GUILogic : MonoBehaviour {
 		GAME.gameBoard = SERIALIZER.loadGameState(state, GAME);
 		Debug.Log ("Received a state");
 		//TODO: Test this works
-		Text end_turn = GameObject.Find("ButtonEndTurn").GetComponentsInChildren<Text>();
+		Text end_turn = GameObject.Find("ButtonEndTurn").GetComponentsInChildren<Text>()[0]; // not sure if this is ben's intent
 		if (GAME.turnOf.username.Equals(NETWORK.GetLocalPlayerName())) {
 			//TODO: Covey it is your turn to the user (Need error message thingy)
 			Debug.Log(GAME.turnOf.username + " " + (GAME.turnOf.myVillages != null).ToString());
