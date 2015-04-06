a45bf9543156a00acb9902517954d42a1fc5019d
Benjamin Hockley <benjamin.hockley@mail.mcgill.ca>
2015-04-06 15:32:45 -0400

    GUI
    Implementation of GUI for joining, loading, and starting a new game is
    done

--- UnityWorkspace/Assets/MW_Scripts/GUI/GameSelect.cs
+++ UnityWorkspace/Assets/MW_Scripts/GUI/GameSelect.cs
@@ -3,25 +3,31 @@ using System.Collections;
 
 public class GameSelect : MonoBehaviour {
 	//Default to first in list
-	public int START_SELECT = 0;
-	public string[] GAMES = new string[] {"radio1", "radio2", "radio3"};
-
+	public string[] GAMES;
+	private Vector2 ListScrollPos;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
 	void OnGUI() {
-		GUILayout.BeginVertical("Box");
-		START_SELECT = GUILayout.SelectionGrid(START_SELECT, GAMES, 1);
-		if (GUILayout.Button("Start"))
-			Debug.Log("You chose " + GAMES[START_SELECT]);
-		
-		GUILayout.EndVertical();
+		CanvasGroup show_menu = this.transform.parent.GetComponentInParent<CanvasGroup>();
+		//Only display this menu if we're showing it
+		if(show_menu.alpha == 1 && show_menu.interactable && show_menu.blocksRaycasts) {
+			GUILayout.BeginArea(new Rect(370, 231.5f, 655, 509.4f));
+			GUILayout.BeginScrollView(ListScrollPos, false, true);
+			GUILayout.BeginVertical(GUILayout.Width(625));
+			foreach(string current in GAMES)
+			{
+				GUILayout.Button(current, GUILayout.Height (40));
+				GUILayout.Space (5);
+			}
+			GUILayout.EndVertical();
+			GUILayout.EndScrollView();
+			GUILayout.EndArea();
+		}
 	}
 }
