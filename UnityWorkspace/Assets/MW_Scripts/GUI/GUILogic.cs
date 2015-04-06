c0b9d9a8a64797a07f446440d4de56a68f38b0ee
Benjamin Hockley <benjamin.hockley@mail.mcgill.ca>
2015-04-06 01:39:20 -0400

    GUILogic
    Made a change before code was changed

--- UnityWorkspace/Assets/MW_Scripts/GUI/GUILogic.cs
+++ UnityWorkspace/Assets/MW_Scripts/GUI/GUILogic.cs
@@ -344,20 +344,17 @@ public class GUILogic : MonoBehaviour {
 			return;
 		}
 		AbstractUnit new_villager = new Unit(building_tile.myVillage, building_tile);
-		if(GAME.myGameLogic.hireVillager(new_villager, building_tile.myVillage, building_tile)) {
-			//Load new unit
-			GameObject new_unit = (GameObject)Resources.Load("peasent");
-			GameObject hired_villager = (GameObject)GameObject.Instantiate(new_unit ,LAST_CLICKED_ON.position + new Vector3(0,0,0), Quaternion.identity);
-			//Set as child
-			hired_villager.transform.parent = GameObject.Find("map").transform;
-			//Destroy current unit
-			Object.Destroy(LAST_CLICKED_ON);
-			//Set new last clicked on
-			LAST_CLICKED_ON = new_unit.transform;
-		}
-		else {
-			//Show error
-		}
+		//TODO: Needs to return a bool so I can handle any issues
+		GAME.myGameLogic.hireVillager(new_villager, building_tile.myVillage, building_tile);
+		//Load new unit
+		GameObject new_unit = (GameObject)Resources.Load("peasent");
+		GameObject hired_villager = (GameObject)GameObject.Instantiate(new_unit ,LAST_CLICKED_ON.position + new Vector3(0,0,0), Quaternion.identity);
+		//Set as child
+		hired_villager.transform.parent = GameObject.Find("map").transform;
+		//Destroy current unit
+		Object.Destroy(LAST_CLICKED_ON);
+		//Set new last clicked on
+		LAST_CLICKED_ON = new_unit.transform;
 	}
 
 	public void moveUnit(Transform tile) {
