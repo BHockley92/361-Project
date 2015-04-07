057ab468cab76cf3eb9e4c7e8731aa000e8c2775
AlexSincennes <alexandre.sincennes@hotmail.com>
2015-04-06 23:53:44 -0400

    removed roundsplayed
    since roundsplayed is just turnindex of AbstractGame

--- UnityWorkspace/Assets/MW_Scripts/ImplementedClasses/MW_Game.cs
+++ UnityWorkspace/Assets/MW_Scripts/ImplementedClasses/MW_Game.cs
@@ -4,22 +4,14 @@ using GameEnums;
 
 public class MW_Game : AbstractGame
 {
-	// Global stats tracked, not for each player
-	public int roundsPlayed { get; set; }
-
 	// for a randomly generated board, assumes length and width are > 0
 	public MW_Game(List<AbstractPlayer> players, AbstractGameLogic gl)
 	{
 		initialize (players, gl);
-		roundsPlayed = 0;
 	}
 
 	public void EndTurn() {
 		nextTurn ();
-		if(turnIndex % participants.Count == 0) {
-			roundsPlayed++;
-			Debug.Log ("Rounds played: " + roundsPlayed);
-		}
 			
 	}
 }
