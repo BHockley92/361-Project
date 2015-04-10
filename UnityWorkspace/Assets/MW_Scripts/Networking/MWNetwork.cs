b988232dbd83dfdb8f6d41e1a7d30b9f862be3ec
lordkevinthebold <kevinbourassahoule@gmail.com>
2015-04-10 03:28:36 -0400

    Displaying name on GameGUI (amazing!!)

--- UnityWorkspace/Assets/MW_Scripts/Networking/MWNetwork.cs
+++ UnityWorkspace/Assets/MW_Scripts/Networking/MWNetwork.cs
@@ -330,6 +330,8 @@ public class MWNetwork : Photon.MonoBehaviour
 	private void OnLoginResult(LoginResult result)
 	{
 		PhotonNetwork.playerName = gui.PLAYER.username;
+		GameObject.Find("Main Camera/Canvas/GameGUI/SelectedInfo/WrittenInfo").GetComponent<Text>().text = "Name: " + gui.PLAYER.username;
+		
 		PlayFabData.AuthKey = PlayFabClientAPI.AuthKey;
 		Debug.Log("Login success!");
 		
